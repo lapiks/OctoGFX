6ebe48219cbf7500497b7e1bcca9cbcb7ac7064f
lapiks <dhenin.julien@gmail.com>
2023-11-15 11:58:46 +0100

    Basic WebGPU setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cassert>
+#include <vector>
 
 #include <GLFW/glfw3.h>
 #include <glfw3webgpu.h>
@@ -50,6 +51,36 @@ WGPUAdapter requestAdapter(WGPUInstance instance, WGPURequestAdapterOptions cons
   return userData.adapter;
 }
 
+WGPUDevice requestDevice(WGPUAdapter adapter, WGPUDeviceDescriptor const* descriptor) {
+  struct UserData {
+    WGPUDevice device = nullptr;
+    bool requestEnded = false;
+  };
+  UserData userData;
+
+  auto onDeviceRequestEnded = [](WGPURequestDeviceStatus status, WGPUDevice device, char const* message, void* pUserData) {
+    UserData& userData = *reinterpret_cast<UserData*>(pUserData);
+    if (status == WGPURequestDeviceStatus_Success) {
+      userData.device = device;
+    }
+    else {
+      std::cout << "Could not get WebGPU device: " << message << std::endl;
+    }
+    userData.requestEnded = true;
+    };
+
+  wgpuAdapterRequestDevice(
+    adapter,
+    descriptor,
+    onDeviceRequestEnded,
+    (void*)&userData
+  );
+
+  assert(userData.requestEnded);
+
+  return userData.device;
+}
+
 int main(int, char**) {
   if (!glfwInit()) {
     std::cerr << "Could not initialize GLFW!" << std::endl;
@@ -57,6 +88,7 @@ int main(int, char**) {
   }
 
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
   GLFWwindow* window = glfwCreateWindow(640, 480, "Voxel Engine", NULL, NULL);
   if (!window) {
@@ -83,12 +115,119 @@ int main(int, char**) {
   WGPUAdapter adapter = requestAdapter(instance, &adapterOpts);
   std::cout << "Got adapter: " << adapter << std::endl;
 
+  std::vector<WGPUFeatureName> features;
+    
+  // Call the function a first time with a null return address, just to get
+  // the entry count.
+  size_t featureCount = wgpuAdapterEnumerateFeatures(adapter, nullptr);
+
+  // Allocate memory (could be a new, or a malloc() if this were a C program)
+  features.resize(featureCount);
+
+  // Call the function a second time, with a non-null return address
+  wgpuAdapterEnumerateFeatures(adapter, features.data());
+
+  std::cout << "Adapter features:" << std::endl;
+  for (auto f : features) {
+    std::cout << " - " << f << std::endl;
+  }
+
+  std::cout << "Requesting device..." << std::endl;
+  WGPUDeviceDescriptor deviceDesc = {};
+  deviceDesc.nextInChain = nullptr;
+  deviceDesc.label = "Device"; // anything works here, that's your call
+  deviceDesc.requiredFeaturesCount = 0; // we do not require any specific feature
+  deviceDesc.requiredLimits = nullptr; // we do not require any specific limit
+  deviceDesc.defaultQueue.nextInChain = nullptr;
+  deviceDesc.defaultQueue.label = "Default queue";
+  WGPUDevice device = requestDevice(adapter, &deviceDesc);
+  std::cout << "Got device: " << device << std::endl;
+
+  auto onDeviceError = [](WGPUErrorType type, char const* message, void* /* pUserData */) {
+    std::cout << "Uncaptured device error: type " << type;
+    if (message) std::cout << " (" << message << ")";
+    std::cout << std::endl;
+    };
+  wgpuDeviceSetUncapturedErrorCallback(device, onDeviceError, nullptr /* pUserData */);
+
+  WGPUQueue queue = wgpuDeviceGetQueue(device);
+
+  WGPUCommandEncoderDescriptor encoderDesc = {};
+  encoderDesc.nextInChain = nullptr;
+  encoderDesc.label = "Command encoder";
+  WGPUCommandEncoder encoder = wgpuDeviceCreateCommandEncoder(device, &encoderDesc);
+
+  wgpuCommandEncoderInsertDebugMarker(encoder, "Do one thing");
+  wgpuCommandEncoderInsertDebugMarker(encoder, "Do another thing");
+
+  WGPUCommandBufferDescriptor cmdBufferDescriptor = {};
+  cmdBufferDescriptor.nextInChain = nullptr;
+  cmdBufferDescriptor.label = "Command buffer";
+  WGPUCommandBuffer command = wgpuCommandEncoderFinish(encoder, &cmdBufferDescriptor);
+
+  // Finally submit the command queue
+  std::cout << "Submitting command..." << std::endl;
+  wgpuQueueSubmit(queue, 1, &command);
+
+#ifdef WEBGPU_BACKEND_DAWN
+  wgpuCommandEncoderRelease(encoder);
+  wgpuCommandBufferRelease(command);
+#endif
+
+  WGPUSwapChainDescriptor swapChainDesc = {};
+  swapChainDesc.nextInChain = nullptr;
+  swapChainDesc.width = 640;
+  swapChainDesc.height = 480;
+  swapChainDesc.usage = WGPUTextureUsage_RenderAttachment;
+  swapChainDesc.presentMode = WGPUPresentMode_Fifo;
+
+#ifdef WEBGPU_BACKEND_DAWN
+  swapChainDesc.format = WGPUTextureFormat_BGRA8Unorm;
+#else 
+  swapChainDesc.format = wgpuSurfaceGetPreferredFormat(surface, adapter);
+#endif
+
+  WGPUSwapChain swapChain = wgpuDeviceCreateSwapChain(device, surface, &swapChainDesc);
+  std::cout << "Swapchain: " << swapChain << std::endl;
+
   while (!glfwWindowShouldClose(window)) {
     // Check whether the user clicked on the close button (and any other
     // mouse/key event, which we don't use so far)
     glfwPollEvents();
+
+    WGPUTextureView nextTexture = wgpuSwapChainGetCurrentTextureView(swapChain);
+    std::cout << "nextTexture: " << nextTexture << std::endl;
+
+    if (!nextTexture) {
+      std::cerr << "Cannot acquire next swap chain texture" << std::endl;
+      break;
+    }
+
+    WGPURenderPassColorAttachment renderPassColorAttachment = {};
+    renderPassColorAttachment.view = nextTexture;
+    renderPassColorAttachment.resolveTarget = nullptr;
+    renderPassColorAttachment.loadOp = WGPULoadOp_Clear;
+    renderPassColorAttachment.storeOp = WGPUStoreOp_Store;
+    renderPassColorAttachment.clearValue = WGPUColor{ 0.9, 0.1, 0.2, 1.0 };
+
+    WGPURenderPassDescriptor renderPassDesc = {};
+    renderPassDesc.colorAttachmentCount = 1;
+    renderPassDesc.colorAttachments = &renderPassColorAttachment;
+    renderPassDesc.depthStencilAttachment = nullptr;
+    renderPassDesc.timestampWriteCount = 0; // for measurements
+    renderPassDesc.timestampWrites = nullptr; // for measurements
+    renderPassDesc.nextInChain = nullptr;
+
+    WGPURenderPassEncoder renderPass = wgpuCommandEncoderBeginRenderPass(encoder, &renderPassDesc);
+    wgpuRenderPassEncoderEnd(renderPass);
+
+    wgpuTextureViewRelease(nextTexture);
+
+    wgpuSwapChainPresent(swapChain);
   }
 
+  wgpuSwapChainRelease(swapChain);
+  wgpuDeviceRelease(device);
   wgpuSurfaceRelease(surface);
   wgpuAdapterRelease(adapter);
   wgpuInstanceRelease(instance);
